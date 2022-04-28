8ec6ab00c3f5f5be6befd3a31c3732b7ba85aa17
gussm07 <78830028+gussm07@users.noreply.github.com>
2022-04-27 20:10:51 -0500

    Update index.js

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -11,7 +11,6 @@ import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './index.scss'
-import ReactIcon from '../../assets/images/react-icon.svg'
 
 const About = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
