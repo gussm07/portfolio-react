074720f21757bc0942e35f560627205903422df4
gussm07 <78830028+gussm07@users.noreply.github.com>
2022-04-27 20:16:40 -0500

    Update index.js

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -9,7 +9,7 @@ import './index.scss'
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
 
-  const nameArray = ['l', 'o', 'b', 'o', 'd', 'a', 'n']
+ const nameArray = ['a', 'n', 'd', 'o', 'v', 'a', 'l']
   const jobArray = [
     'w',
     'e',
