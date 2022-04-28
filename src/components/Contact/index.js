cad5351f5d6e54dcd49187d48972773d3d50c805
gussm07 <78830028+gussm07@users.noreply.github.com>
2022-04-27 20:00:58 -0500

    Update index.js

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -8,6 +8,7 @@ import './index.scss'
 
 const Contact = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
+  /* REF PARA EL FORMULARIO DE EMAIL */
   const form = useRef()
 
   useEffect(() => {
@@ -19,12 +20,15 @@ const Contact = () => {
   const sendEmail = (e) => {
     e.preventDefault()
 
+    /* GMAIL ES UN TEMPLATE CREADO EN email.js
+    template_bh44hka ES EN EMAIL TEMPLATE EN Template ID 
+    el lt_ ESTA EN INTEGRATION-API KEYS*/
     emailjs
       .sendForm(
-        'gmail',
-        'template_YeJhZkgb',
+        'service_2gjf0i2',
+        'template_bh44hka',
         form.current,
-        'your-token'
+        'lt_ZADxs1sUTrE0D2'
       )
       .then(
         () => {
@@ -54,8 +58,10 @@ const Contact = () => {
             don't hesitate to contact me using below form either.
           </p>
           <div className="contact-form">
+            {/* SE USA LA LIBRARIA email.js */}
             <form ref={form} onSubmit={sendEmail}>
               <ul>
+                {/* "half" parte a la mitad la columna */}
                 <li className="half">
                   <input placeholder="Name" type="text" name="name" required />
                 </li>
@@ -90,20 +96,23 @@ const Contact = () => {
           </div>
         </div>
         <div className="info-map">
-          Slobodan Gajić,
+          Gustavo Sandoval,
           <br />
-          Serbia,
+          México,
           <br />
-          Branka RadiČevića 19, 22000 <br />
-          Sremska Mitrovica <br />
+          Tlaxcala <br />
+          Chiautempan <br />
           <br />
-          <span>freelancerslobodan@gmail.com</span>
+          <span>gus_sm07@hotmail.com</span>
         </div>
         <div className="map-wrap">
-          <MapContainer center={[44.96366, 19.61045]} zoom={13}>
+          <MapContainer
+            center={[19.28607264286604, -98.16644485814463]}
+            zoom={8}
+          >
             <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-            <Marker position={[44.96366, 19.61045]}>
-              <Popup>Sloba lives here, come over for a cup of coffee :)</Popup>
+            <Marker position={[19.317400606249823, -98.19108544022545]}>
+              <Popup>Gus lives here, come over for a cup of coffee :)</Popup>
             </Marker>
           </MapContainer>
         </div>
