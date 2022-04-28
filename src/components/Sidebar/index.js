bd80099ae8d64d5b52fec204d347bfd32e3f047a
gussm07 <78830028+gussm07@users.noreply.github.com>
2022-04-28 12:01:22 -0500

    Update index.js
    Update contact info

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -36,7 +36,7 @@ const Sidebar = () => {
       <ul>
         <li>
           <a
-            href="https://www.linkedin.com/in/slobodan-gaji%C4%87-006bb8b8/"
+            href="https://www.linkedin.com/in/gustavo-sandoval-morales-6a8211108/"
             target="_blank"
             rel="noreferrer"
           >
@@ -45,27 +45,15 @@ const Sidebar = () => {
         </li>
         <li>
           <a
-            href="https://github.com/bobangajicsm"
+            href="https://github.com/gussm07"
             target="_blank"
             rel="noreferrer"
           >
             <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
           </a>
         </li>
-        <li>
-          <a
-            href="https://www.youtube.com/channel/UCBu5ulO4d-d47lAVybpRTkw"
-            rel="noreferrer"
-            target="_blank"
-          >
-            <FontAwesomeIcon icon={faYoutube} color="#4d4d4e" />
-          </a>
-        </li>
-        <li>
-          <a href="skype:live:bobangajicsm" rel="noreferrer" target="_blank">
-            <FontAwesomeIcon icon={faSkype} color="#4d4d4e" />
-          </a>
-        </li>
+     
+        
       </ul>
     </div>
   )
