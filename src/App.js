30637842b6d56afdbfa7a37ad37ef10fc8de5d11
gussm07 <78830028+gussm07@users.noreply.github.com>
2022-05-02 14:54:26 -0500

    Update App.js
    Adding Skills and Portfolio pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { Route, Routes } from 'react-router-dom'
+import React, { Component } from 'react'
 import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
 import Layout from './components/Layout'
 import './App.scss'
+import Portfolio from './components/Portfolio'
+import Skills from './components/Skills'
 
 function App() {
   return (
@@ -12,7 +15,9 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
+          <Route path="/portfolio" element={<Portfolio />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/skills" element={<Skills />} />
         </Route>
       </Routes>
     </>
